a90663acc1c44db94c34ce0ae0ea59b1443ce8a6
AmanUndre <amanundre8@gmail.com>
2026-02-12 16:19:59 +0530

    Existing code

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,24 +4,25 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// define __dirname in ESM
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [ react(), tailwindcss() ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
+  server: isDevelopment ? {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your backend port
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
     },
-  },
+  } : undefined,
 })
